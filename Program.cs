e16a4fe99b63efc10f435f804cdd7a4a0165d96a
Jenni <jennifergds89@gmail.com>
2023-03-17 21:08:53 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("CO");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor =ConsoleColor.DarkRed;
+Console.Write("RIN");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.DarkRed; 
+Console.Write("THI");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor =ConsoleColor.DarkRed;
+Console.Write("ANS ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("<3");
+Console.ResetColor();
